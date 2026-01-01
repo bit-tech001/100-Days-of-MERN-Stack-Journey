300bab270ac8dc9b1f3b5fd481f8a2683b6456ad
bit-tech001 <tasus0833@gmail.com>
2026-01-02 02:01:50 +0530

    Initial commit - Day 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import Introduction from './Day1/Introduction'
+
+
+const App = () => {
+  return (
+     <> 
+     <Introduction/>  
+     </>
+  )
+}
+
+export default App
---
+++ src/Frontend - Day1/Introduction.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Introduction() {
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+        <div className="prose prose-gray lg:prose-xl text-center">
+          <h1 className="font-bold tracking-tight text-gray-900 text-4xl">
+            Day 1 of 100 Days of MERN Stack 🚀
+          </h1>
+          <p className="text-gray-600 p-6">
+            Building consistency, logic, and real-world skills step by step.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Introduction;
